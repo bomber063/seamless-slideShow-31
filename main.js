a8521c91e7a1cb9764a0078c24d3da43467ddb5b
bomber <tangyihong063@163.com>
2019-05-13 19:58:08 +0800

    修改代码以达到长时间都可以无缝轮播

--- main.js
+++ main.js
@@ -2,64 +2,64 @@ $('.imgs>img:nth-child(1)').addClass('current')
 $('.imgs>img:nth-child(2)').addClass('enter')
 $('.imgs>img:nth-child(3)').addClass('enter')
 
-// var s = 3
-// var n
-// setInterval(() => {
-//         n = (s % 3)+1
+var s = 3
+var n
+setInterval(() => {
+        n = (s % 3)+1
     
-//     $(`.imgs>img:nth-child(${n})`).removeClass('current enter').addClass('leave').
-//         one('transitionend', function (e) {
-//             $(e.currentTarget).removeClass('leave current').addClass('enter')//这里把e.currentTarget写成了'e.currentTarget'，整了半天才发现错误。
-//             // console.log(e.currentTarget)
-//             //removeClass()可以同时移除多个class
-//         })
-//     if (n + 1 > 3) {
-//         n = 0
-//         $(`.imgs>img:nth-child(${n + 1})`).removeClass('enter leave').addClass('current')
-//     }
+    $(`.imgs>img:nth-child(${n})`).removeClass('current enter').addClass('leave').
+        one('transitionend', function (e) {
+            $(e.currentTarget).removeClass('leave current').addClass('enter')//这里把e.currentTarget写成了'e.currentTarget'，整了半天才发现错误。
+            // console.log(e.currentTarget)
+            //removeClass()可以同时移除多个class
+        })
+    if (n + 1 > 3) {
+        n = 0
+        $(`.imgs>img:nth-child(${n + 1})`).removeClass('enter leave').addClass('current')
+    }//这里用到的反引号``
 
-//     if (n + 1 <= 3) {
-//         $(`.imgs>img:nth-child(${n + 1})`).removeClass('enter leave').addClass('current')
-//     }
+    if (n + 1 <= 3) {
+        $(`.imgs>img:nth-child(${n + 1})`).removeClass('enter leave').addClass('current')
+    }//这里不可以让n+1超过3,这里一直都是1,2——2,3——3,1——1,2这样循环。
 
-//     s = s + 1
-// }, 1000);
+    s = s + 1
+}, 1000);
 
-setTimeout(() => {
-    $('.imgs>img:nth-child(1)').removeClass('current enter').addClass('leave').
-    one('transitionend',function(e){
-        $(e.currentTarget).removeClass('leave current').addClass('enter')//这里把e.currentTarget写成了'e.currentTarget'，整了半天才发现错误。
-        console.log(e.currentTarget)//removeClass()可以同时移除多个class
-    })
-    $('.imgs>img:nth-child(2)').removeClass('enter current').addClass('current')
-}, 2000)
+// setTimeout(() => {
+//     $('.imgs>img:nth-child(1)').removeClass('current enter').addClass('leave').
+//     one('transitionend',function(e){
+//         $(e.currentTarget).removeClass('leave current').addClass('enter')//这里把e.currentTarget写成了'e.currentTarget'，整了半天才发现错误。
+//         console.log(e.currentTarget)//removeClass()可以同时移除多个class
+//     })
+//     $('.imgs>img:nth-child(2)').removeClass('enter current').addClass('current')
+// }, 2000)
 
-setTimeout(() => {
-    $('.imgs>img:nth-child(2)').removeClass('current enter').addClass('leave').
-    one('transitionend',function(e){
-        $(e.currentTarget).removeClass('leave current').addClass('enter')//这里把e.currentTarget写成了'e.currentTarget'，整了半天才发现错误。
-        console.log(e.currentTarget)
-    })
-    $('.imgs>img:nth-child(3)').removeClass('enter current').addClass('current')
-}, 4000)
+// setTimeout(() => {
+//     $('.imgs>img:nth-child(2)').removeClass('current enter').addClass('leave').
+//     one('transitionend',function(e){
+//         $(e.currentTarget).removeClass('leave current').addClass('enter')//这里把e.currentTarget写成了'e.currentTarget'，整了半天才发现错误。
+//         console.log(e.currentTarget)
+//     })
+//     $('.imgs>img:nth-child(3)').removeClass('enter current').addClass('current')
+// }, 4000)
 
-setTimeout(() => {
-    $('.imgs>img:nth-child(3)').removeClass('current enter').addClass('leave').
-    one('transitionend',function(e){
-        $(e.currentTarget).removeClass('leave current').addClass('enter')//这里把e.currentTarget写成了'e.currentTarget'，整了半天才发现错误。
-        console.log(e.currentTarget)
-    })
-    $('.imgs>img:nth-child(1)').removeClass('enter current').addClass('current')
-}, 6000)
+// setTimeout(() => {
+//     $('.imgs>img:nth-child(3)').removeClass('current enter').addClass('leave').
+//     one('transitionend',function(e){
+//         $(e.currentTarget).removeClass('leave current').addClass('enter')//这里把e.currentTarget写成了'e.currentTarget'，整了半天才发现错误。
+//         console.log(e.currentTarget)
+//     })
+//     $('.imgs>img:nth-child(1)').removeClass('enter current').addClass('current')
+// }, 6000)
 
-setTimeout(() => {
-    $('.imgs>img:nth-child(1)').removeClass('current enter').addClass('leave').
-    one('transitionend',function(e){
-        $(e.currentTarget).removeClass('current leave').addClass('enter')//这里把e.currentTarget写成了'e.currentTarget'，整了半天才发现错误。
-        console.log(e.currentTarget)//removeClass()可以同时移除多个class
-    })
-    $('.imgs>img:nth-child(2)').removeClass('enter current').addClass('current')
-}, 8000)
+// setTimeout(() => {
+//     $('.imgs>img:nth-child(1)').removeClass('current enter').addClass('leave').
+//     one('transitionend',function(e){
+//         $(e.currentTarget).removeClass('current leave').addClass('enter')//这里把e.currentTarget写成了'e.currentTarget'，整了半天才发现错误。
+//         console.log(e.currentTarget)//removeClass()可以同时移除多个class
+//     })
+//     $('.imgs>img:nth-child(2)').removeClass('enter current').addClass('current')
+// }, 8000)
 
 
 
